7396ec88712256d7091c1bc7b0b0e4565ae051ac
Ma-Kas <115688479+Ma-kas@users.noreply.github.com>
2023-05-25 18:11:58 +0900

    Complete base assignment
    Fully working website according to assignment specifications;
    
    Next steps: Code cleanup, implement eraser, implement color picker,
    implement toggle for grid lines;

--- js/index.js
+++ js/index.js
@@ -1,9 +1,22 @@
-const gridSize = 16; // Initial gridSize definition
+let gridSize = 16; // Initial gridSize definition
+let mouseDown = false;
 
 
 // Get references to DOM elements
+const body = document.querySelector('body');
 const gridContainer = document.querySelector('.container');
 const setGridButton = document.querySelector('#grid-btn');
+const clearGridButton = document.querySelector('#clear-btn');
+
+
+// Toggle mouseDown variable to enable or disable drawing ability
+body.addEventListener('mousedown', () => {
+  mouseDown = true;
+});
+
+body.addEventListener('mouseup', () => {
+  mouseDown = false;
+});
 
 
 // Get css width of container holding the grid
@@ -16,13 +29,19 @@ function drawGrid(size = gridSize) {
   let cellSize = containerSize / size;
   for (let i = 0; i < (size ** 2); i++) {
     const gridCell = document.createElement('div');
+
     gridCell.classList.add('cell');
     gridCell.style.cssText = `width: ${cellSize}px; height: ${cellSize}px;`;
     gridContainer.appendChild(gridCell);
+
+    gridCell.addEventListener('mouseover', (e) => {
+      colorHoveredCell(e, gridCell)
+    });
+    gridCell.addEventListener('mousedown', (e) => {
+      colorHoveredCell(e, gridCell)
+    });
   }
 }
-
-drawGrid(gridSize);
 
 
 // Delete old grid by removing all children(=cells) from grid Container
@@ -33,15 +52,35 @@ function deleteGrid() {
 }
 
 
-// On mouse over, add the hovered class to gridCell
+// On mouse over while mousedown, add the hovered class to gridCell
+function colorHoveredCell(e, cell) {
+    if (e.type === 'mouseover' && !mouseDown) {
+      return; // return condition if mouse is not down
+    }
+    cell.classList.add('hovered');
+}
 
-// On click
+
+// Change grid through button
 setGridButton.addEventListener('click', () => {
   let newGridSize = prompt("New Grid Size");
   if (isNaN(newGridSize) || (newGridSize < 1) || (newGridSize > 100)) {
     return;
   } else {
     deleteGrid();
-    drawGrid(newGridSize);
+    gridSize = newGridSize;
+    drawGrid(gridSize);
   }
 });
+
+
+// Clear current drawing through button
+clearGridButton.addEventListener('click', () => {
+  let cells = document.querySelectorAll('.cell');
+
+  for (cell of cells) {
+    cell.classList.remove('hovered');
+  }
+})
+
+drawGrid(gridSize);
